199c8ed164ab19c3ce8dc1a182a2a6253f04b17d
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-13 14:34:53 +0200

    wip
    different images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="">
         <div class="">
-            <img src="{{ asset('images/students.jpg') }}" alt="Students" class="h-96 w-full" />
+            <img src="{{ asset('images/stu.jpg') }}" alt="Students" class="h-96 w-full" />
         </div>
     </div>
 </x-layout>

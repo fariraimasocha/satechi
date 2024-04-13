e32e0f5af27e7dfe6b9cb5814ca692180fb121b0
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-13 14:04:22 +0200

    wip
    top nav completed

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -2,7 +2,11 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between">
             <h1 class="text-white text-sm">Whatsapp:061 486 5651 | Email:info@satechi.co.zw | Tirelo ICT Park Mabulela Atok</h1>
-            <h1 class="text-white text-sm">MY ACCOUNT</h1>
+            <div class="flex space-x-1">
+                <div class="rounded-full w-3 h-3 bg-white mt-1">
+                </div>
+                <h1 class="text-white text-sm">MY ACCOUNT</h1>
+            </div>
         </div>
     </div>
 </nav>

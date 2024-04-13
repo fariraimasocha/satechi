8f73a85cf086f3a3dfe1242c64a5ceaeabd699e6
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-13 13:57:31 +0200

    feat
    ailgn top nav items

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <h1 class="text-center font-extrabold text-2xl">Top Navigation</h1>
-</div>
+<nav class="bg-blue1 py-2">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between">
+            <h1 class="text-white text-sm">Whatsapp:061 486 5651 | Email:info@satechi.co.zw | Tirelo ICT Park Mabulela Atok</h1>
+            <h1 class="text-white text-sm">MY ACCOUNT</h1>
+        </div>
+    </div>
+</nav>

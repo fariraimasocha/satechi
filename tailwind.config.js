72d56c9ec97c267474b30b2bee8d6f5a350122a7
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-13 13:34:21 +0200

    feat
    add Fortify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+/* @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/protonemedia/laravel-splade/lib/**/*.vue",
+        "./vendor/protonemedia/laravel-splade/resources/views/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
+        // "./app/Forms/*.php",
+        // "./app/Tables/*.php",
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            colors : {
+                black1: '#fbc33b',
+                blue1: '#003087',
+
+            },
+        },
     },
-    plugins: [],
-}
+
+    plugins: [forms, typography],
+};

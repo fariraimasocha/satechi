d967d417fec399756311edf0a41805ba6d3e1e6c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-23 09:17:41 +0200

    wip
    top nav

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -3,7 +3,7 @@
         <div class="flex justify-between">
             <h1 class="text-white text-sm">Whatsapp:061 486 5651 | Email:info@satechi.co.zw | Tirelo ICT Park Mabulela Atok</h1>
             <div class="flex space-x-1">
-                <div class="rounded-full w-3 h-3 bg-white mt-1">
+                <div class="rounded-full w-3 h-3 bg-white mt-2">
                 </div>
                 <a href="{{route('login')}}" >
                 <button class="text-white text-sm">LOGIN</button>

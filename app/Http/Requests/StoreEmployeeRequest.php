b1b64269cefb56c015909fbfd640d87f8acc5beb
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-28 12:47:48 +0200

    feat
    employee backend

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -11,7 +11,7 @@ class StoreEmployeeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreEmployeeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|between:2,100',
+            'position' => 'required|string|between:2,100',
+            'department' => 'required|string|between:2,100',
+            'address' => 'required|string|between:2,100',
         ];
     }
 }

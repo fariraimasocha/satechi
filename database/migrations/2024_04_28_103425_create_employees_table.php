fb8fe40d209973a27193701ae17254a192c6df05
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-28 12:39:19 +0200

    Update 2024_04_28_103425_create_employees_table.php
    wip

--- database/migrations/2024_04_28_103425_create_employees_table.php
+++ database/migrations/2024_04_28_103425_create_employees_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('department');
+            $table->string('address');
             $table->timestamps();
         });
     }

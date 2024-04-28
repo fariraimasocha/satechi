ed0f20bfd3788bb62442754a52f8754d4d064ec0
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-04-28 13:37:46 +0200

    wip
    add auth redirect to home

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CollegeTeamController;
 use App\Http\Controllers\CommunityController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ServicesController;
@@ -26,6 +27,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['splade'])->group(function () {
     Route::get('/', fn () => view('home'))->name('home');
+
+    Route::middleware(['auth'])->group(function () {
+        Route::resource('/home', HomeController::class);
+    });
 
     Route::resource('/apply', ApplyController::class);
     Route::resource('/courses', CourseController::class);
